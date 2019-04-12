4a7a6b8f1033801e9ba4ad53da392231ec6a2285
Liane-Tsun Horn <205505307@stu.ukzn.ac.za>
2019-04-12 23:37:02 +0200

    SalesRepToComplete

--- MenuForms/MenuForms/CFO.vb
+++ MenuForms/MenuForms/CFO.vb
@@ -1,3 +1,9 @@
 ﻿Public Class CFO
+    Private Sub CaptureSalesOrderTab_Click(sender As Object, e As EventArgs) Handles CaptureSalesOrderTab.Click
 
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
 End Class
