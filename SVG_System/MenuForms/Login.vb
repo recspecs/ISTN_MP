60c703992e1abc05d8841a1ba880798da9380849
ScThTony <tonymfb13@gmail.com>
2019-07-09 23:24:18 +0200

    m

--- SVG_System/MenuForms/Login.vb
+++ SVG_System/MenuForms/Login.vb
@@ -1,21 +1,21 @@
 ﻿Imports System.Data.SqlClient
 Public Class Login
-    Dim form1 As New GenManager
-    Dim form2 As New SalesRep
-    Dim form3 As New WarehouseManager
+    Dim genManagerForm As New GenManager
+    Dim salesRepForm As New SalesRep
+    Dim warehouseManagerForm As New WarehouseManager
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         EmployeeTableAdapter1.FillBy(RecSpecDataset1.Employee, TextBoxEmail.Text, TextBoxPassword.Text)
         If RecSpecDataset1.Employee.Rows.Count > 0 Then
             MessageBox.Show("Welcome " & RecSpecDataset1.Employee.Rows(0).Item(1).ToString & vbLf & "You will now be directed to the system")
             If RecSpecDataset1.Employee.Rows(0).Item(6).ToString = "General Manager" Then
-                form1.ShowDialog()
+                genManagerForm.Show()
                 Me.Close()
             ElseIf RecSpecDataset1.Employee.Rows(0).Item(6).ToString = "Sales Rep" Then
-                form2.ShowDialog()
+                salesRepForm.Show()
                 Me.Close()
             ElseIf RecSpecDataset1.Employee.Rows(0).Item(6).ToString = "Warehouse Manager" Then
-                form3.ShowDialog()
+                warehouseManagerForm.Show()
                 Me.Close()
 
 
